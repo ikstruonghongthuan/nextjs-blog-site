c5dc9c1adbc29b298da3f7435f94b16c90a0cc95
ikstruonghongthuan <ikstruonghongthuan@gmail.com>
2023-12-19 02:24:47 +0700

    here we go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { CircularProgress } from "@mui/material";
+
 import BlogCard from "@/components/BlogCard";
+
 import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <main className={styles["blog-container"]}>
+        <CircularProgress className="text-white" size={60} />
         <BlogCard />
         <BlogCard />
         <BlogCard />
